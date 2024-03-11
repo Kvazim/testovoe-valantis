eb6c5a2097991294012a3ba47297de501681f092
Kvazim <mesvladimir@gmail.com>
2024-03-11 17:42:56 +0300

    fix: правка useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,161 +1,117 @@
 import { useState, useEffect } from 'react';
-import { createAPI } from './services/api';
 import { MAX_PRODUCT_PER_PAGE } from './const';
 import { Container, Pagination, Stack} from '@mui/material';
 import { getProducts } from './services/api-products';
 
-const Loaded = () => {
+function Loaded() {
   return <div>Loading........</div>;
 }
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  // const [filteredProducts, setFilteredProducts] = useState([]);
-  // const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [page, setPage] = useState(1);
   const [pageQty, setPageQty] = useState(0);
-  // const [totalPages, setTotalPages] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
 
-  const getProductsId = async (postParams = null) => {
-    const api = createAPI();
+  const getAllProductsId = async () => {
+    const allProducts = await getProducts('get_ids');
+    const fullProducts = new Set();
 
-    try {
-      const response = await api.post('', {action: 'get_ids', ...postParams});
-      const fullProducts = new Set();
+    allProducts.forEach((id) => {
+      fullProducts.add(id)
+    });
 
-      response.data.result.forEach((id) => {
-        fullProducts.add(id)
-      });
+    setPageQty(Math.ceil(Array.from(fullProducts.keys()).length / MAX_PRODUCT_PER_PAGE))
+  }
 
-      return Array.from(fullProducts.keys());
-    } catch (error) {
-        throw new Error(error);
+  const getLimitedProducts = async (currentPage) => {
+    const getCurrentProducts = await getProducts('get_ids', {params: { offset: (currentPage - 1) * MAX_PRODUCT_PER_PAGE, limit: MAX_PRODUCT_PER_PAGE }});
+    const getCurrentProductsByid = await getProducts('get_items', {params: { ids: getCurrentProducts }});
+    const filteredProducts = new Map();
+    getCurrentProductsByid.forEach((product) => {
+      if (!filteredProducts.has(product.id)) {
+        filteredProducts.set(product.id, product);
       }
+    });
+
+    setProducts(Array.from(filteredProducts.values()));
   }
 
-  const getCurrentProducts = async (currentProducts) => {
-    const api = createAPI();
+  useEffect(() => {
+    getAllProductsId();
+  }, []);
 
-    try {
-      const response = await api.post('', { action: 'get_items', params: { ids: currentProducts } });
-      const filteredProducts = new Map();
-      response.data.result.forEach((product) => {
-        if (!filteredProducts.has(product.id)) {
-          filteredProducts.set(product.id, product);
-        }
-      });
+  useEffect(() => {
+    setIsLoading(true);
+    getLimitedProducts(page).finally(setIsLoading(false));
+  }, [page]);
 
-      return Array.from(filteredProducts.values());
-    } catch (error) {
-      throw new Error(error);
-    }
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSearchSubmit = (event) => {
+        event.preventDefault();
+        // handleFilter();
+    };
+
+  if(isLoading) {
+    return <Loaded />
   }
 
-    useEffect(() => {
-        const getAllProducts = async () => {
-            const allProducts = await getProductsId();
-            setPageQty(Math.ceil(allProducts.length / MAX_PRODUCT_PER_PAGE))
-        }
-
-        getAllProducts();
-
-        const getTest = async () => {
-          const testItem = await getProducts('get_fields', {params: {field: "brand"}});
-          console.log(testItem);
-        }
-
-        getTest();
-    },[])
-
-    useEffect(() => {
-        setIsLoading(true);
-
-        const getLimitedProducts = async () => {
-            const limitedProducts = await getProductsId({params: { offset: (page - 1) * MAX_PRODUCT_PER_PAGE, limit: MAX_PRODUCT_PER_PAGE }});
-            const paginatedProducts = await getCurrentProducts(limitedProducts)
-            setProducts(paginatedProducts);
-            setIsLoading(false);
-        }
-
-        getLimitedProducts();
-    }, [page])
-    // console.log(products);
-
-    // const handleSearchChange = (event) => {
-    //     // setSearchTerm(event.target.value);
-    // };
-
-    // const handleSearchSubmit = (event) => {
-    //     event.preventDefault();
-    //     // handleFilter();
-    // };
-
-    if(isLoading) {
-        return <Loaded />
-    }
-
-    return (
-        <div>
-            <h1>Product List</h1>
-            {products.length > 0 && (
-                <ul style={{display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '25px'}}>
-                    {products.map(product => (
-                        <li key={product.id}>
-                            {product.id && (
-                                <div style={{marginBottom: '15px'}}>
-                                    ID: <span>{product.id}</span>
-                                </div>
-                            )}
-                            {product.product && (
-                                <div style={{marginBottom: '15px'}}>
-                                    PRODUCT: <span>{product.product}</span>
-                                </div>
-                            )}
-                            {product.brand && (
-                                <div style={{marginBottom: '15px'}}>
-                                    BRAND: <span>{product.brand}</span>
-                                </div>
-                            )}
-                            {product.price && (
-                                <div>
-                                    PRICE: <span>{product.price}$</span>
-                                </div>
-                            )}
-                        </li>
-                    ))}
-                </ul>
-            )}
-            <Container sx={{marginTop: 5}}>
-                <Stack spacing={2}>
-                    {
-                        !!pageQty && (
-                            <Pagination
-                                count={pageQty}
-                                page={page}
-                                onChange={(_, num) => setPage(num) }
-                            />
-                    )}
-                </Stack>
-            </Container>
-            {/* <form onSubmit={handleSearchSubmit}>
-                <input type="text" value={searchTerm} onChange={handleSearchChange} />
-                <button type="submit">Search</button>
-            </form> */}
-            {/* {
-                <ul>
-                    {products.map(product => (
-                        <li style={{marginBottom: '30px'}} key={product.id}>
-                            {product.id && <div style={{marginBottom: '15px'}}>ID: <div>{product.id}</div></div>}
-                            {product.product && <div style={{marginBottom: '15px'}}>PRODUCT: <div>{product.product}</div></div>}
-                            {product.brand && <div style={{marginBottom: '15px'}}>BRAND: <div>{product.brand}</div></div>}
-                            {product.price && <div>PRICE: {product.price}$</div>}
-                        </li>
-                    ))}
-                </ul>
-            } */}
-        </div>
-    );
+  return (
+    <>
+      <h1>Product List</h1>
+      <div>{products.length}</div>
+        <form onSubmit={handleSearchSubmit}>
+          <input type="text" value={searchTerm} onChange={handleSearchChange} />
+          <button type="submit">Search</button>
+        </form>
+      {products.length > 0 && (
+        <ul style={{display: 'grid', gridTemplateColumns: 'repeat(2, 1fr', rowGap: '25px', columnGap: '50px'}}>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.id && (
+                <div style={{marginBottom: '15px'}}>
+                  ID: <span>{product.id}</span>
+                </div>
+              )}
+              {product.product && (
+                <div style={{marginBottom: '15px'}}>
+                  PRODUCT: <span>{product.product}</span>
+                </div>
+              )}
+              {product.brand && (
+                <div style={{marginBottom: '15px'}}>
+                  BRAND: <span>{product.brand}</span>
+                </div>
+              )}
+              {product.price && (
+                <div>
+                  PRICE: <span>{product.price}$</span>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+      <Container sx={{marginTop: 5}}>
+        <Stack spacing={2}>
+          {
+            !!pageQty && (
+              <Pagination
+                count={pageQty}
+                page={page}
+                onChange={(_, num) => setPage(num) }
+              />
+            )
+          }
+        </Stack>
+      </Container>
+    </>
+  );
 };
 
 export default App;
--- src/services/api-products.js
+++ src/services/api-products.js
@@ -1,12 +1,20 @@
 import { createAPI } from './api';
 
-export const getProducts = async (actionType, postParams = null) => {
-  const api = createAPI();
+const api = createAPI();
 
+export const getProducts = async (actionType, postParams = null) => {
   try {
     const response = await api.post('', {action: actionType, ...postParams});
     return response.data.result
   } catch (error) {
-      throw new Error(error);
+    if (error.response) {
+      console.error('Server Error:', error.response.data);
+    } else if (error.request) {
+      console.error('Request Error:', error.request);
+    } else {
+      console.error('Error:', error.message);
     }
+
+    throw error;
+  }
 }
