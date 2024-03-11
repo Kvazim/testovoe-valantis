7d743e4d803af8187eda8fb9d0b11b5172b895d3
Vladimir <mesvladimir@yandex.ru>
2024-03-12 00:09:00 +0300

    fix: правка загрузки

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { MAX_PRODUCT_PER_PAGE } from './const';
 import { Container, Pagination, Stack} from '@mui/material';
 import { getProducts } from './services/api-products';
@@ -12,6 +12,8 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [page, setPage] = useState(1);
   const [pageQty, setPageQty] = useState(0);
+  const [differensProduct, setDifferensProduct] = useState(0);
+  const [differensLimit, setDifferensLimit] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
   const getAllProductsId = async () => {
@@ -25,18 +27,30 @@ const App = () => {
     setPageQty(Math.ceil(Array.from(fullProducts.keys()).length / MAX_PRODUCT_PER_PAGE))
   }
 
-  const getLimitedProducts = async (currentPage) => {
-    const getCurrentProducts = await getProducts('get_ids', {params: { offset: (currentPage - 1) * MAX_PRODUCT_PER_PAGE, limit: MAX_PRODUCT_PER_PAGE }});
+  const getLimitedProducts = useCallback(async (currentPage) => {
+    let getCurrentProducts = await getProducts('get_ids', {params: { offset: (currentPage - 1) * (MAX_PRODUCT_PER_PAGE + differensProduct), limit: MAX_PRODUCT_PER_PAGE + differensLimit }});
+
     const getCurrentProductsByid = await getProducts('get_items', {params: { ids: getCurrentProducts }});
     const filteredProducts = new Map();
+
     getCurrentProductsByid.forEach((product) => {
       if (!filteredProducts.has(product.id)) {
         filteredProducts.set(product.id, product);
       }
     });
 
-    setProducts(Array.from(filteredProducts.values()));
-  }
+    const optimizedProducts = Array.from(filteredProducts.values())
+
+    if (optimizedProducts.length < MAX_PRODUCT_PER_PAGE) {
+      const differensProduct = MAX_PRODUCT_PER_PAGE - optimizedProducts.length;
+      setDifferensProduct((prev) => prev + differensProduct);
+      setDifferensLimit((prev) => prev + differensProduct);
+      return;
+    }
+
+    setDifferensLimit(0);
+    setProducts(optimizedProducts);
+  }, [differensLimit, differensProduct]);
 
   const getFilteredProducts = async (searchType, value) => {
     const filteredProducts = await getProducts('filter', {params: {[searchType]: value}});
@@ -49,8 +63,8 @@ const App = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    getLimitedProducts(page).finally(setIsLoading(false));
-  }, [page]);
+    getLimitedProducts(page).finally(() => setIsLoading(false));
+  }, [getLimitedProducts, page]);
 
 
     const handleSearchChange = (event) => {
